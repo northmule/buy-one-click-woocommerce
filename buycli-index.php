5997f7b4cdb3d1f72de085ef419303a86538eb45
northmule <izm@zixn.ru>
2021-09-02 10:29:40 +0300

    fix comment

--- buycli-index.php
+++ buycli-index.php
@@ -8,9 +8,9 @@
  * Author: Djo
  * Author URI: https://zixn.ru
  * WC requires at least: 3.9
- * WC tested up to: 5.2
+ * WC tested up to: 5.6
  * Requires at least: 5.1
- * Tested up to: 5.7
+ * Tested up to: 5.8
  * Text Domain: coderun-oneclickwoo
  * Domain Path: /languages
  */
